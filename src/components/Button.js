d84e381c43f2846b99bac6f42d11fa87d645448e
Stuart Glass <stuartglass@WKS64231.Emap-intl.net>
2019-10-18 12:04:38 +0100

    added deps

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import PropTypes from 'prop-types'
+
+const Wrapper = styled.button({
+  background: '#1EA7FD',
+  color: 'white',
+  padding: '10px 16px'
+})
+
+/**
+- In code comment JS-DOC 
+- The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
+**/
+const Button = ({text}) => (
+  <Wrapper>{text}</Wrapper>
+)
+
+Button.propTypes = {
+  /** Button text */
+  text: PropTypes.string.isRequired,
+}
+
+
+export default Button
